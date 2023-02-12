fe3140c098210d1a7c0f3a3962c74ff1083c6bbc
pgtung <pgtung@gmail.com>
2023-02-12 12:53:16 +0700

    upgrade Paypal-SDK - fix bug

--- includes/paypal-checkout/request-payment.php
+++ includes/paypal-checkout/request-payment.php
@@ -280,7 +280,7 @@ $payment->setIntent("sale")
     ->setRedirectUrls($redirectUrls)
     ->setTransactions(array($transaction))
     ->setExperienceProfileId($profileId);
-    print_r(json_encode($payment));
+    echo json_encode(json_decode($payment));
 try {
     $payment->create($apiContext);
 } catch (PayPal\Exception\PayPalConnectionException $ex) {
