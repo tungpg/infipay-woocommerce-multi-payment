cdd59eb7d1cc39ad613d412b95f2a81f92352a0c
pgtung <pgtung@gmail.com>
2023-04-07 22:38:35 +0700

    support airwallex

--- includes/awx-form/awx-split-card-form.php
+++ includes/awx-form/awx-split-card-form.php
@@ -4,24 +4,40 @@
 <head>
 <link rel='dns-prefetch' href='//checkout.airwallex.com' />
 <link rel='stylesheet' id='airwallex-css-css' href='<?php echo esc_url(plugin_dir_url(__DIR__))?>assets/css/airwallex-checkout.css?ver=6.0.3' media='all' />
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js' id='jquery-core-js'></script>
+<script
+	src='https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.3.2/jquery-migrate.min.js'
+	id='jquery-migrate-js'></script>
+<script type='text/javascript'
+	src='https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js'
+	id='jquery-blockui-js'></script>
+<script
+	src='https://checkout.airwallex.com/assets/elements.bundle.min.js?ver=6.0.3'
+	id='airwallex-lib-js-js'></script>
+<script
+	src="<?php echo esc_url(plugin_dir_url(__DIR__))?>assets/js/airwallex-checkout.js?a=<?php echo time();?>"
+	id='airwallex-local-js-js'></script>
+
 </head>
 
 <body>
 
-	<div id="airwallex-card"></div>
-	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js' id='jquery-core-js'></script>
-	<script
-		src='https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.3.2/jquery-migrate.min.js'
-		id='jquery-migrate-js'></script>
-	<script type='text/javascript'
-		src='https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js'
-		id='jquery-blockui-js'></script>
-	<script
-		src='https://checkout.airwallex.com/assets/elements.bundle.min.js?ver=6.0.3'
-		id='airwallex-lib-js-js'></script>
-	<script
-		src="<?php echo esc_url(plugin_dir_url(__DIR__))?>assets/js/airwallex-checkout.js?a=<?php echo time();?>"
-		id='airwallex-local-js-js'></script>
+    <!-- Add empty containers for each card input element  -->
+    <div style={containerStyle}>
+        <div>Card number</div>
+        <div id="cardNumber"></div>
+      </div>
+      <div style={containerStyle}>
+        <div>Expiry</div>
+        <div id="expiry"></div>
+      </div>
+      <div style={containerStyle}>
+        <div>CVC</div>
+        <div id="cvc"></div>
+    </div>
+	
+	
 
 	<script id='airwallex-local-js-js-after'>
             const AirwallexParameters = {
@@ -58,7 +74,7 @@
             // });
 
             function handleSubmit(formData) {
-                parent.postMessage("mecom-startSubmitPaymentStripe", "*");
+                parent.postMessage("infipay-startSubmitPaymentAirwallex", "*");
                 confirmSlimCardPayment(0, formData);
             }
 
@@ -68,23 +84,25 @@
                 origin: window.location.origin, // Setup your event target to receive the browser events message
             });
 
-            const airwallexSlimCard = Airwallex.createElement('card', {
-                style: {
-                    popupWidth: 400,
-                    popupHeight: 549,
-                },
-            });
+         	// Create split card elements
+            const cardNumber = Airwallex.createElement('cardNumber');
+            const expiry = Airwallex.createElement('expiry');
+            const cvc = Airwallex.createElement('cvc');
 
-            airwallexSlimCard.mount('airwallex-card');
-            setInterval(function() {
-                if (document.getElementById('airwallex-card') && !document.querySelector('#airwallex-card iframe')) {
-                    try {
-                        airwallexSlimCard.mount('airwallex-card')
-                    } catch {
+            // Mount split card elements
+            cardNumber.mount('cardNumber'); // Injects iframe into the Card Number Element container
+            expiry.mount('expiry'); // Injects iframe into the Expiry Element container
+            cvc.mount('cvc'); // Injects iframe into the CVC Element container
+            
+//             setInterval(function() {
+//                 if (document.getElementById('airwallex-card') && !document.querySelector('#airwallex-card iframe')) {
+//                     try {
+//                         airwallexSlimCard.mount('airwallex-card')
+//                     } catch {
 
-                    }
-                }
-            }, 1000);
+//                     }
+//                 }
+//             }, 1000);
 
             function confirmSlimCardPayment(orderId, formData) {
                 //timeout necessary because of event order in plugin CheckoutWC
@@ -106,12 +124,12 @@
                 AirwallexClient.ajaxPost(asyncIntentUrl, dataPost, function(data) {
                     if (!data || data.error) {
                         parent.postMessage({
-                            name: "mecom-errorSubmitPaymentStripe",
+                            name: "infipay-errorSubmitPaymentAirwallex",
                             value: String('An error has occurred. Please check your payment details (%s)').replace('%s', '')
                         }, "*");
                     }
                     parent.postMessage({
-                            name: "mecom-endSubmitPaymentStripe",
+                            name: "infipay-endSubmitPaymentAirwallex",
                             value: data,
                             datarq: {'paymentIntent':data.paymentIntent,'clientSecret':data.clientSecret,
                                 card: {
@@ -122,7 +140,7 @@
                         }, "*");
                     //send message to client checkout
                     Airwallex.confirmPaymentIntent({
-                        element: airwallexSlimCard,
+                        element: cardNumber,
                         id: data.paymentIntent,
                         client_secret: data.clientSecret,
                         payment_method: {
@@ -139,13 +157,13 @@
                     }).then((response) => {
                         // location.href = finalConfirmationUrl;
                         parent.postMessage({
-                            name: "mecom-endSubmitPaymentStripe",
+                            name: "infipay-endSubmitPaymentAirwallex",
                             value: response,
                             datarq: data
                         }, "*");
                     }).catch(err => {
                         parent.postMessage({
-                            name: "mecom-errorSubmitPaymentStripe",
+                            name: "infipay-errorSubmitPaymentAirwallex",
                             value: err,
                             datarq: data
                         }, "*");
@@ -184,7 +202,7 @@
             }
 
             function listener(event) {
-                "object" == typeof event.data && "mecom-submitFormStripe" === event.data.name && handleSubmit(event.data.value)
+                "object" == typeof event.data && "infipay-submitFormAirwallex" === event.data.name && handleSubmit(event.data.value)
             }
             window.addEventListener ? window.addEventListener("message", listener) : window.attachEvent("onmessage", listener);
             window.addEventListener('onError', (event) => {
