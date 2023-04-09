73294415423c02cb05caf7377f18c8cb206ac42b
pgtung <pgtung@gmail.com>
2023-04-09 19:07:22 +0700

    support airwallex

---
+++ includes/awx-form/InfipayAirwallexCardClient.php
@@ -0,0 +1,111 @@
+<?php
+use Airwallex\CardClient;
+
+class InfipayAirwallexCardClient extends CardClient
+{
+    final public function createPaymentIntentExt($dataPayment, $withDetails = false){
+        $client = $this->getHttpClient();
+        // $orderNumber = ($orderNumber = $order->get_meta('_order_number')) ? $orderNumber : $orderId;
+        $customerId=null;
+        $data = [
+            'amount' => $dataPayment['totalprice'],
+            'currency' => 'USD',
+            'descriptor' => 'order #'.$dataPayment['payment_id'].'-'.$dataPayment['payment_order_id'] ,
+            'metadata' => [
+                'wp_order_id' => $dataPayment['payment_id'],
+            ],
+            'merchant_order_id' => $dataPayment['payment_id'],
+            'return_url' => WC()->api_request_url('airwallex_payment_process'),//WC()->api_request_url(Main::ROUTE_SLUG_CONFIRMATION),
+            'order' => [
+                'type' => 'physical_goods',
+            ],
+            'request_id' => uniqid(),
+        ]
+        + ($customerId !== null ? ['customer_id' => $customerId] : []);
+        
+        if (mb_strlen($data['descriptor']) > 32) {
+            $data['descriptor'] = mb_substr($data['descriptor'], 0, 32);
+        }
+        
+        // Set customer detail
+        $customerAddress = [
+            'city'=> $dataPayment['city'],
+            'country_code' => $dataPayment['country'],
+            'postcode' => $dataPayment['zipcode'],
+            'state' => $dataPayment['state'],
+            'street' => $dataPayment['address'],
+        ];
+        
+        $customer = [
+            'email' => $dataPayment['email'],
+            'first_name' => $dataPayment['fname'],
+            'last_name' => $dataPayment['lname'],
+            'merchant_customer_id' => $dataPayment['payment_id'],
+            'phone_number' =>$dataPayment['phone'],
+            'address' => [
+                'city' => $dataPayment['city'],
+                'country_code' => $dataPayment['country'],
+                'postcode' => $dataPayment['zipcode'],
+                'state' => $dataPayment['state'],
+                'street' => $dataPayment['address'],
+            ],
+        ];
+        
+        $data['customer'] = $customerId === null ? $customer : null;
+        
+        // Set order details
+        $orderData = [
+            'type' => 'physical_goods',
+            'products' => [],
+        ];
+        
+        $orderData['products'][] = [
+            'desc' => $dataPayment['payment_code'],
+            'name' => $dataPayment['payment_code'],
+            'quantity' => 1,
+            'sku' => $dataPayment['payment_code'],
+            'type' => 'physical',
+            'unit_price' => round($dataPayment['totalprice'], 2),
+        ];
+        
+        $orderData['shipping'] = [
+            'address' => [
+                'city' => $dataPayment['city'],
+                'country_code' => $dataPayment['country'],
+                'postcode' => $dataPayment['zipcode'],
+                'state' => $dataPayment['state'],
+                'street' => $dataPayment['address'],
+            ],
+            'first_name' => $dataPayment['fname'],
+            'last_name' => $dataPayment['lname'],
+            'shipping_method' => 'standard',
+        ];
+        //var_dump($orderData);die;
+        $data['order'] = $orderData;
+        // echo 'getReferrer:'. json_encode($this->getReferrer()).'<br>';
+        // echo 'getToken:'.$this->getToken().'<br>';
+        // echo 'getAuthorizationRetryClosure:'.json_encode($this->getAuthorizationRetryClosure()).'<br>';
+        // echo json_encode(
+        //     $data
+        //     + $this->getReferrer()
+        // );
+        $response = $client->call(
+            'POST',
+            $this->getPciUrl('pa/payment_intents/create'),
+            json_encode(
+                $data
+                + $this->getReferrer()
+                ),
+            [
+                'Authorization' => 'Bearer ' . $this->getToken(),
+            ],
+            $this->getAuthorizationRetryClosure()
+            );
+        
+        if (empty($response->data['id'])) {
+            throw new Exception('payment intent creation failed: ' . json_encode($response));
+        }
+        
+        return new PaymentIntent($response->data);
+    }
+}
--- includes/awx-form/awx-process.php
+++ includes/awx-form/awx-process.php
@@ -1,71 +1,49 @@
 <?php
-use Airwallex\CardClient;
+use InfipayAirwallexCardClient; // Object from Airwallex Woocommerce Official Plugin
 
 header('Content-Type: application/json');
 http_response_code(200);
 
 try {
     
-//     $data = array(
-//         'payment_order_id'    => isset($_POST['payment_order_id']) ? $_POST['payment_order_id'] : null,
-//         'clientname'    => isset($_POST['clientname']) ? $_POST['clientname'] : null,
-//         'payment_code' => isset($_POST['payment_code']) ? $_POST['payment_code'] : null,
+    $data = array(
+        'payment_order_id'    => isset($_POST['payment_order_id']) ? $_POST['payment_order_id'] : null,
+        'clientname'    => isset($_POST['clientname']) ? $_POST['clientname'] : null,
+        'payment_code' => isset($_POST['payment_code']) ? $_POST['payment_code'] : null,
         
-//         'fname' => isset($_POST['first_name']) ? $_POST['first_name'] : null,
-//         'lname' => isset($_POST['last_name']) ? $_POST['last_name'] : null,
-//         'country' => isset($_POST['country']) ? $_POST['country'] : null,
-//         'address' => isset($_POST['line1']) ? $_POST['line1'] : null,
-//         'city' => isset($_POST['city']) ? $_POST['city'] : null,
-//         'state' => isset($_POST['state']) ? $_POST['state'] : null,
-//         'zipcode' => isset($_POST['postal_code']) ? $_POST['postal_code'] : null,
-//         'phone' => isset($_POST['phone']) ? $_POST['phone'] : null,
-//         'email' => isset($_POST['email']) ? $_POST['email'] : null,
+        'fname' => isset($_POST['first_name']) ? $_POST['first_name'] : null,
+        'lname' => isset($_POST['last_name']) ? $_POST['last_name'] : null,
+        'country' => isset($_POST['country']) ? $_POST['country'] : null,
+        'address' => isset($_POST['line1']) ? $_POST['line1'] : null,
+        'city' => isset($_POST['city']) ? $_POST['city'] : null,
+        'state' => isset($_POST['state']) ? $_POST['state'] : null,
+        'zipcode' => isset($_POST['postal_code']) ? $_POST['postal_code'] : null,
+        'phone' => isset($_POST['phone']) ? $_POST['phone'] : null,
+        'email' => isset($_POST['email']) ? $_POST['email'] : null,
         
-//         'totalprice' => isset($_POST['totalprice']) ? $_POST['totalprice'] : null,
-//         'pagecheckout' => isset($_POST['pagecheckout']) ? $_POST['pagecheckout'] : null,
-//         'pagethankyou' => isset($_POST['pagethankyou']) ? $_POST['pagethankyou'] : null,
+        'totalprice' => isset($_POST['totalprice']) ? $_POST['totalprice'] : null,
+        'pagecheckout' => isset($_POST['pagecheckout']) ? $_POST['pagecheckout'] : null,
+        'pagethankyou' => isset($_POST['pagethankyou']) ? $_POST['pagethankyou'] : null,
         
-//         'airwallex_consent_id' => null,
-//         'airwallex_customer_id' => null,
-//         'noteorder' => null,
-//         'statuspayment' => isset($_POST['statuspayment']) ? $_POST['statuspayment'] : null,
-//     );
+        'airwallex_consent_id' => null,
+        'airwallex_customer_id' => null,
+        'noteorder' => null,
+        'statuspayment' => isset($_POST['statuspayment']) ? $_POST['statuspayment'] : null,
+    );
     
-    $apiClient = new Airwallex\CardClient();
-    
-    http_response_code(200);
+    $apiClient = new InfipayAirwallexCardClient();
     
     print_r($apiClient);
-    echo json_encode([
-        'error' => 1,
-    ]);
-    die;
-    // if (!empty($_GET['airwallexOrderId'])) {
-    //     $payment_id = $_GET['airwallexOrderId'];
-    //     WC()->session->set('airwallex_order', $payment_id);
-    // }
-    // if (empty($payment_id)) {
-    //     $payment_id = (int)WC()->session->get('airwallex_order');
-    // }
-    // if (empty($payment_id)) {
-    //     $payment_id = (int)WC()->session->get('order_awaiting_payment');
-    // }
+    
+    header('Content-Type: application/json');
+    http_response_code(200);
+    die();
+    
     $gateway = new Airwallex\Gateways\Card();
-    // $order = wc_get_order($orderId);
-    // if (empty($order)) {
-    //     throw new Exception('Order not found: ' . $orderId);
-    // }
     $orderService = new Airwallex\Services\OrderService();
     $airwallexCustomerId = null;
-    // if ($orderService->containsSubscription($order->get_id())) {
-    //     $airwallexCustomerId = $orderService->getAirwallexCustomerId($order->get_customer_id(''), $apiClient);
-    // }
-    // $logService->debug('asyncIntent() before create', ['orderId' => $orderId]);
     $paymentIntent = $apiClient->createPaymentIntentExt($paymentProcess);
     WC()->session->set('airwallex_payment_intent_id', $paymentIntent->getId());
-    // $order['airwallex_consent_id'] = $paymentIntent->getPaymentConsentId();
-    // $order['airwallex_customer_id'] = $paymentIntent->getCustomerId();
-    // update_post_meta($orderId, '_tmp_airwallex_payment_intent', $paymentIntent->getId());
     
     header('Content-Type: application/json');
     http_response_code(200);
@@ -79,13 +57,7 @@ try {
         'airwallex_customer_id'=>$paymentIntent->getCustomerId(),
         'clientSecret' => $paymentIntent->getClientSecret(),
     ];
-    // $logService->debug('asyncIntent() response', [
-    //     'response' => $response,
-    //     'session' => [
-    //         'cookie' => WC()->session->get_session_cookie(),
-    //         'data' => WC()->session->get_session_data(),
-    //     ],
-    // ]);
+    
     echo json_encode($response);
     die;
 } catch (Exception $e) {
