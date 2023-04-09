a52a0ce506622a29269b5356c9345e77b3c206a1
pgtung <pgtung@gmail.com>
2023-04-09 15:58:54 +0700

    support airwallex

--- includes/awx-form/awx-process.php
+++ includes/awx-form/awx-process.php
@@ -6,37 +6,30 @@ http_response_code(200);
 
 try {
     
-    $data = array(
-        'payment_order_id'    => isset($_POST['payment_order_id']) ? $_POST['payment_order_id'] : null,
-        'clientname'    => isset($_POST['clientname']) ? $_POST['clientname'] : null,
-        'payment_code' => isset($_POST['payment_code']) ? $_POST['payment_code'] : null,
+//     $data = array(
+//         'payment_order_id'    => isset($_POST['payment_order_id']) ? $_POST['payment_order_id'] : null,
+//         'clientname'    => isset($_POST['clientname']) ? $_POST['clientname'] : null,
+//         'payment_code' => isset($_POST['payment_code']) ? $_POST['payment_code'] : null,
         
-        'fname' => isset($_POST['first_name']) ? $_POST['first_name'] : null,
-        'lname' => isset($_POST['last_name']) ? $_POST['last_name'] : null,
-        'country' => isset($_POST['country']) ? $_POST['country'] : null,
-        'address' => isset($_POST['line1']) ? $_POST['line1'] : null,
-        'city' => isset($_POST['city']) ? $_POST['city'] : null,
-        'state' => isset($_POST['state']) ? $_POST['state'] : null,
-        'zipcode' => isset($_POST['postal_code']) ? $_POST['postal_code'] : null,
-        'phone' => isset($_POST['phone']) ? $_POST['phone'] : null,
-        'email' => isset($_POST['email']) ? $_POST['email'] : null,
+//         'fname' => isset($_POST['first_name']) ? $_POST['first_name'] : null,
+//         'lname' => isset($_POST['last_name']) ? $_POST['last_name'] : null,
+//         'country' => isset($_POST['country']) ? $_POST['country'] : null,
+//         'address' => isset($_POST['line1']) ? $_POST['line1'] : null,
+//         'city' => isset($_POST['city']) ? $_POST['city'] : null,
+//         'state' => isset($_POST['state']) ? $_POST['state'] : null,
+//         'zipcode' => isset($_POST['postal_code']) ? $_POST['postal_code'] : null,
+//         'phone' => isset($_POST['phone']) ? $_POST['phone'] : null,
+//         'email' => isset($_POST['email']) ? $_POST['email'] : null,
         
-        'totalprice' => isset($_POST['totalprice']) ? $_POST['totalprice'] : null,
-        'pagecheckout' => isset($_POST['pagecheckout']) ? $_POST['pagecheckout'] : null,
-        'pagethankyou' => isset($_POST['pagethankyou']) ? $_POST['pagethankyou'] : null,
+//         'totalprice' => isset($_POST['totalprice']) ? $_POST['totalprice'] : null,
+//         'pagecheckout' => isset($_POST['pagecheckout']) ? $_POST['pagecheckout'] : null,
+//         'pagethankyou' => isset($_POST['pagethankyou']) ? $_POST['pagethankyou'] : null,
         
-        'airwallex_consent_id' => null,
-        'airwallex_customer_id' => null,
-        'noteorder' => null,
-        'statuspayment' => isset($_POST['statuspayment']) ? $_POST['statuspayment'] : null,
-    );
-    $db = new PaymentServerDB();
-    
-    $paymentProcess = $db->getPaymentClientbyCode($data['payment_code']);
-    if(($paymentProcess)){
-        $db->deletePaymentClientsbycode($data['payment_code']);
-    }
-    $paymentProcess = $db->createPaymentClients($data);
+//         'airwallex_consent_id' => null,
+//         'airwallex_customer_id' => null,
+//         'noteorder' => null,
+//         'statuspayment' => isset($_POST['statuspayment']) ? $_POST['statuspayment'] : null,
+//     );
     
     $apiClient = new Airwallex\CardClient();
     
