5e94032ea1dfcc67734f34c3b93087ab5093249b
tungpg <pgtung@gmail.com>
2023-04-10 15:54:52 +0700

    update

--- assets/js/airwallex-checkout.js
+++ assets/js/airwallex-checkout.js
@@ -60,8 +60,8 @@ const AirwallexClient = {
         xmlhttp.send();
     },
     ajaxPost: function (url, datapost, callback) {
-        alert(JSON.stringify(datapost));
-/*
+/*        alert(JSON.stringify(datapost));
+
 		$.ajax({
             url: url,
             data: datapost,
--- includes/checkout.php
+++ includes/checkout.php
@@ -69,7 +69,35 @@ if (isset($_GET) ) {
 	} else if(isset($_GET['infipay-awx-get-payment-form-split-card'])) {
 	    include dirname( __FILE__ ) . '/awx-form/awx-split-card-form.php';
 	} elseif (isset($_GET['infipay-awx-make-payment'])) {
-	    include dirname( __FILE__ ) . '/awx-form/awx-process.php';
+	    $data = array(
+	        'payment_order_id'    => isset($postData['payment_order_id']) ? $postData['payment_order_id'] : null,
+	        'clientname'    => isset($postData['clientname']) ? $postData['clientname'] : null,
+	        'payment_code' => isset($postData['payment_code']) ? $postData['payment_code'] : null,
+	        
+	        'fname' => isset($postData['first_name']) ? $postData['first_name'] : null,
+	        'lname' => isset($postData['last_name']) ? $postData['last_name'] : null,
+	        'country' => isset($postData['country']) ? $postData['country'] : null,
+	        'address' => isset($postData['line1']) ? $postData['line1'] : null,
+	        'city' => isset($postData['city']) ? $postData['city'] : null,
+	        'state' => isset($postData['state']) ? $postData['state'] : null,
+	        'zipcode' => isset($postData['postal_code']) ? $postData['postal_code'] : null,
+	        'phone' => isset($postData['phone']) ? $postData['phone'] : null,
+	        'email' => isset($postData['email']) ? $postData['email'] : null,
+	        
+	        'totalprice' => isset($postData['totalprice']) ? $postData['totalprice'] : null,
+	        'pagecheckout' => isset($postData['pagecheckout']) ? $postData['pagecheckout'] : null,
+	        'pagethankyou' => isset($postData['pagethankyou']) ? $postData['pagethankyou'] : null,
+	        
+	        'airwallex_consent_id' => null,
+	        'airwallex_customer_id' => null,
+	        'noteorder' => null,
+	        'statuspayment' => isset($postData['statuspayment']) ? $postData['statuspayment'] : null,
+	    );
+	    header('Content-Type: application/json');
+	    http_response_code(200);
+	    echo json_encode($data);
+	    die();
+	    //include dirname( __FILE__ ) . '/awx-form/awx-process.php';
 	} elseif (isset($_GET['infipay-awx-confirm-payment'])) {
 	    include dirname( __FILE__ ) . '/awx-form/awx-confirm-payment.php';	    
 	    
