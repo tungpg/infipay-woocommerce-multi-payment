a19cff1e9671eff10f0afb46e990e21291f02ad6
pgtung <pgtung@gmail.com>
2023-04-18 06:44:33 +0700

    support airwallex

--- includes/awx-form/awx-form.php
+++ includes/awx-form/awx-form.php
@@ -167,7 +167,7 @@ $infipay_checkout_page_url = get_permalink( get_page_by_path( 'icheckout' ) );
                 }).catch(err => {
                     parent.postMessage({
                         name: "infipay-errorSubmitPaymentAirwallex",
-                        value: err,
+                        value: err.message,
                         datarq: data
                     }, "*");
                     // AirwallexClient.displayCheckoutError(String('An error has occurred. Please check your payment details (%s)').replace('%s', err.message || ''));
