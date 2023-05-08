4a38b861b3f5ed1b5a1ad57395283d40d6f76269
tungpg <pgtung@gmail.com>
2023-05-08 09:17:00 +0700

    fix bug

--- includes/awx-form/awx-form.php
+++ includes/awx-form/awx-form.php
@@ -59,8 +59,6 @@ $infipay_checkout_page_url = get_permalink( get_page_by_path( 'icheckout' ) );
         // });
 
         function handleSubmit(formData) {
-        	alert(JSON.stringify(formData));
-            
             if(is_card_form_valid){
                 parent.postMessage("infipay-startSubmitPaymentAirwallex", "*");
                 confirmSlimCardPayment(formData);
@@ -187,7 +185,7 @@ $infipay_checkout_page_url = get_permalink( get_page_by_path( 'icheckout' ) );
             window.attachEvent("onmessage", listener);
         }
 
-        function listener(event) {            
+        function listener(event) {
             if ((typeof event.data === 'object') && event.data.name === 'infipay-submitFormAirwallex') {
                 handleSubmit(event.data.value);
             }
