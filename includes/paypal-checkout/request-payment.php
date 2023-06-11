ce09a49f2b096676e517b86c30b680ea36063aad
pgtung <pgtung@gmail.com>
2023-06-11 12:02:49 +0700

    fix paypal checkout

--- includes/paypal-checkout/request-payment.php
+++ includes/paypal-checkout/request-payment.php
@@ -286,7 +286,7 @@ try {
     //$error_message = "10736 - A match of the Shipping Address City, State and Postal Code failed.";
     $error_message = $ex->getMessage();
     $error_message .= print_r($ex, true);
-    $error_message .= "AAAAAAA" . print_r($shipping_address, true);
+    $error_message .= "AAAAAAA" . print_r($order_json, true);
     
     echo json_encode([
         'error' => $error_message,
